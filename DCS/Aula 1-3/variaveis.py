1ecd12c338d8f544b0e085cd003d0230843793b2
PedroH <274360868+Manteigas2s@users.noreply.github.com>
2026-04-22 21:28:45 -0300

    Reapply "CDCS"
    This reverts commit 64e492f2ed4f566bf705762e7b3ee2b1d103af83.

---
+++ DCS/Aula 1-3/variaveis.py
@@ -0,0 +1,13 @@
+nome = "Pedro"
+
+# print("Olá,",nome+"!")
+# print(f"Olá, {nome}!")
+
+idade = "20"
+# print(idade)
+
+altura = "1.70m"
+# print(altura)
+
+print(f"Olá, {nome}. Você tem {idade} anos de idade! Com {altura} de altura.")
+
